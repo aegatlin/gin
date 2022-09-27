f5197d3dcb4c82501f69ddc9c684a228075ee1e5
Austin Gatlin <austin@gatlin.io>
2022-09-27 16:05:18 +1000

    Report global gin version after "gin up".

--- dist/up.json.js
+++ dist/up.json.js
@@ -1,5 +1,15 @@
+import { execSync } from 'child_process';
 import { Action } from './action.js';
 export const up = {
     name: 'up',
-    actions: [Action.installDeps(['@aegatlin/gin@latest'], { global: true })],
+    actions: [
+        Action.installDeps(['@aegatlin/gin@latest'], { global: true }),
+        {
+            description: 'Report global gin version.',
+            action: () => {
+                const x = execSync('npm ls -g --depth 0 | grep @aegatlin/gin');
+                console.log(x.toString('utf8'));
+            },
+        },
+    ],
 };
--- lib/up.json.ts
+++ lib/up.json.ts
@@ -1,6 +1,17 @@
+import { execSync } from 'child_process'
 import { Action } from './action.js'
+import { GinCommand } from './types.js'
 
-export const up = {
+export const up: GinCommand = {
   name: 'up',
-  actions: [Action.installDeps(['@aegatlin/gin@latest'], { global: true })],
+  actions: [
+    Action.installDeps(['@aegatlin/gin@latest'], { global: true }),
+    {
+      description: 'Report global gin version.',
+      action: () => {
+        const x = execSync('npm ls -g --depth 0 | grep @aegatlin/gin')
+        console.log(x.toString('utf8'))
+      },
+    },
+  ],
 }
