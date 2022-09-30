d86fca011b559d9e63d3305d18ea3f029d4f29c8
Austin Gatlin <austin@gatlin.io>
2022-09-30 19:28:51 +1000

    Update gin lib.

--- dist/commands/lib.json.js
+++ dist/commands/lib.json.js
@@ -2,15 +2,20 @@ import { Action, ActionDictionary, Actions } from '../action.js';
 import { refPath } from '../utils.js';
 import { asdf } from './asdf.json.js';
 import { prettier } from './prettier.json.js';
+import { skooh } from './skooh.json.js';
 export const lib = {
     name: 'lib',
     actions: [
         ActionDictionary.shellScript.npmInitY,
         ...Actions.fromCommand(asdf, { subCommand: 'node18' }),
         ...Actions.fromCommand(prettier),
+        ...Actions.fromCommand(skooh),
         Action.execShellScript('mkdir lib && touch lib/index.ts'),
         Action.writeFile('./tsconfig.json', refPath('lib/tsconfig.json')),
         Action.writeFile('./.gitignore', refPath('lib/git-ignore')),
         ActionDictionary.shellScript.gitInit,
+        Action.setScript('ppp', 'npm version patch && git push && npm publish'),
+        Action.setScript('build', 'tsc'),
+        Action.setScript('watch', 'tsc --watch'),
     ],
 };
--- lib/commands/lib.json.ts
+++ lib/commands/lib.json.ts
@@ -3,6 +3,7 @@ import { GinCommand } from '../types.js'
 import { refPath } from '../utils.js'
 import { asdf } from './asdf.json.js'
 import { prettier } from './prettier.json.js'
+import { skooh } from './skooh.json.js'
 
 export const lib: GinCommand = {
   name: 'lib',
@@ -10,9 +11,13 @@ export const lib: GinCommand = {
     ActionDictionary.shellScript.npmInitY,
     ...Actions.fromCommand(asdf, { subCommand: 'node18' }),
     ...Actions.fromCommand(prettier),
+    ...Actions.fromCommand(skooh),
     Action.execShellScript('mkdir lib && touch lib/index.ts'),
     Action.writeFile('./tsconfig.json', refPath('lib/tsconfig.json')),
     Action.writeFile('./.gitignore', refPath('lib/git-ignore')),
     ActionDictionary.shellScript.gitInit,
+    Action.setScript('ppp', 'npm version patch && git push && npm publish'),
+    Action.setScript('build', 'tsc'),
+    Action.setScript('watch', 'tsc --watch'),
   ],
 }
